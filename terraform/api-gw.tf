39afbf8a3c38c4335b8dd00f2ea2d92f2c0ccefe
LM Bibera <lyndon.bibera@headhuntr.io>
2021-05-13 13:33:24 +0800

    some notes for the next adventure

--- terraform/api-gw.tf
+++ terraform/api-gw.tf
@@ -3,3 +3,5 @@ resource "aws_apigatewayv2_api" "example" {
   description   = "Entrypoint for the different lambda tech"
   protocol_type = "HTTP"
 }
+
+//TODO: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/api_gateway_integration
